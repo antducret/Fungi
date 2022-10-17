89ba9cce88d7c3240c34b6810f9eaca1ce4dce2b
Anthony Ducret <anthony.ducret@lilo.org>
2022-10-17 14:01:07 +0200

    Initialisation du projet (Load data fonctionne pas)

---
+++ Projet_Rstudio/Main.R
@@ -0,0 +1,12 @@
+
+# PATH ---------------------------------------------------------------
+PATH_OCCURRENCE_TOT = "../Data/occurence_tot.csv"
+PATH_OCCURRENCE_BENTHIC = "../Data/occurence_benthic.csv"
+PATH_OCCURRENCE_PELAGIC = "../Data/occurence_pelagic.csv"
+PATH_ENV = "../Data/env.csv"
+# Load data ---------------------------------------------------------------
+Occ_tot = load(PATH_OCCURRENCE_TOT)
+Occ_benthic = load(PATH_OCCURRENCE_BENTHIC)
+Occ_pelagic = load(PATH_OCCURRENCE_PELAGIC)
+Env = load(PATH_ENV)
+
