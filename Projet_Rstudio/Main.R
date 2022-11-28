9014012b5cb40cf3f6d11e7a661316d4851b0879
Teva Brotherson <teva.brotherson@epfl.ch>
2022-11-28 12:34:16 +0100

    Data exploration + supervised clustering

--- Projet_Rstudio/Main.R
+++ Projet_Rstudio/Main.R
@@ -1,7 +1,9 @@
 
 # PREPARE SESSION ---------------------------------------------------------
+
 # Set directory ?
 # Librairies ?
+setwd(dir = "/home/filsdufrere/Documents/Multivariate Statistics in R/datasets_fungi/Fungi/Projet_Rstudio")
 library(tidyverse)
 
 world_map = map_data("world")
@@ -24,36 +26,17 @@ PATH_ENV = "../Data/env.csv"
     # Total
     spe <- read.csv(PATH_SPE, sep = ',', row.names = NULL)
     name_species <- spe[,2:11]
-    spe <- spe[,-(0:11)]
-    site_names <- colnames(spe[,-1], prefix = "")
-    colnames(spe) <- append(1:56, "Tot", 0) # Pourquoi ?
-  
-    # Pelagic
-    spe.pel <- read.csv(PATH_SPE_PELAGIC, sep = ',', quote = "", row.names = NULL); 
-    spe.pel <- spe.pel[,-(0:11)];
-    colnames(spe.pel) <- append(1:56, "Tot", 0);
-  
-    # Benthic
-    spe.ben <- read.csv(PATH_SPE_BENTHIC, sep = ',', quote = "", row.names = NULL) 
-    spe.ben <- spe.ben[,-(0:11)]
-    colnames(spe.ben) <- append(1:56, "Tot", 0)
-  
-# Data exploration ENV  ---------------------------------------------------------------
-    # Summary 
-      summary(env)
-      str(env) # asfactor pour N/Y sediment ?  
-      
-    # Map sites (TODO : Improve)
-      coo = env[,(1:2)]
-      map <- ggplot(world_map, aes(x = long, y = lat, group = group))  +
-      geom_path()+
-      scale_y_continuous(breaks = (-2:2) * 30) +
-      scale_x_continuous(breaks = (-4:4) * 45) +
-      geom_point(data = coo, aes(x=Longitude..degrees., y=Latitide..degrees., size = sites.pres[-1],colour = "red"), inherit.aes = FALSE) +
-      xlab("° Longitude") + ylab("° Latitude") +
-         labs(colour ='Sites of study',size = "Number of occurrences" , title = "Location of sites")
-map 
     
+    spe <- spe[spe$Class != '',]
+    spe$Class <- as.factor(spe$Class)
+    spe <- aggregate(spe[,-(0:11)], by = list(spe$Class), FUN = "sum")
+    site_names <- colnames(spe[,-1], prefix = "")
+    namecol <- append(1:56, "Tot", 0)
+    colnames(spe) <- append(namecol, "Class", 0) # Pourquoi ?
+    
+    head(spe)
+    
+
 #asp = 1, 
 #type = "p", 
 #main = "Site Locations", 
@@ -73,8 +56,8 @@ map
     # relative presence of each species
             
         # total presence for each species
-          spe.pres <- apply(spe > 0, 1, sum)
-          sites.pres <- apply(spe > 0, 2, sum)
+          spe.pres <- apply(spe[, 2:58]> 0, 1, sum)
+          sites.pres <- apply(spe[, 2:58]> 0, 2, sum)
         # Sort the results in increasing order
           sort(spe.pres)
         
@@ -86,7 +69,93 @@ map
                col = gray(5 : 0 / 5),
                horiz=F,
        )
+        # Data exploration ENV  ---------------------------------------------------------------
+        # Summary 
+        summary(env)
+        str(env) # asfactor pour N/Y sediment ?  
+        
+        # Map sites (TODO : Improve)
+        coo = env[,(1:2)]
+        map <- ggplot(world_map, aes(x = long, y = lat, group = group))  +
+          geom_path()+
+          scale_y_continuous(breaks = (-2:2) * 30) +
+          scale_x_continuous(breaks = (-4:4) * 45) +
+          geom_point(data = coo, aes(x=Longitude..degrees., y=Latitide..degrees., size = sites.pres[-1],colour = "red"), inherit.aes = FALSE) +
+          xlab("° Longitude") + ylab("° Latitude") +
+          labs(colour ='Sites of study',size = "Number of occurrences of classes" , title = "Location of sites")
+        map 
+        
          
 
-# Plot species on map ?? 
+#Supervised classification
+    #Classification trees, Random Forest 
+    # Hierarchical (dendrograms) or not (k-means)
+
+spe.norm = decostand(spe[,2:58], 'normalize')
+
+spe.hel  = vegdist(spe.norm, 'hel') ## psk bcp 0
+
+# Compute and plot complete-linkage agglomerative clustering
+
+spe.hel.complete <- hclust(spe.hel, method = "complete")
+plot(spe.hel.complete, main = "Chord - Complete linkage")
+spe.hel.complete
+
+# Compute UPGMA clustering
+spe.hel.UPGMA <- hclust(spe.hel, method = "average")
+plot(spe.hel.UPGMA, main = "Chord - UPGMA")
+
+
+# Compute UPGMC clustering
+spe.hel.centroid <- hclust(spe.hel, method = "centroid")
+plot(spe.hel.centroid, main = "Chord - Centroid")
+
+
+# Compute Ward's minimum variance clustering
+spe.hel.ward <- hclust(spe.hel, method = "ward.D2")
+plot(spe.hel.ward,  main = "Chord - Ward")
+
+
+#k-means clustering
+spe.hel.k<-  kmeans(spe.hel, centers=5)
+spe.hel.k
+
+
+# Cophenetic correlations -------------------------------------------------
+# Single linkage clustering
+spe.hel.single.coph <- cophenetic(spe.hel.single)
+cor(spe.hel, spe.hel.single.coph)
+
+
+# Complete linkage clustering
+spe.hel.comp.coph <- cophenetic(spe.hel.complete)
+cor(spe.hel, spe.hel.comp.coph)
+
+
+# Average clustering
+spe.hel.UPGMA.coph <- cophenetic(spe.hel.UPGMA)
+cor(spe.hel, spe.hel.UPGMA.coph)
+
+
+# Ward clustering
+spe.hel.ward.coph <- cophenetic(spe.hel.ward)
+cor(spe.hel, spe.hel.ward.coph)
+
+library(NbClust)
+
+
+Nb.complete <-NbClust(spe[,2:58], diss=spe.hel, distance = NULL, min.nc=1, max.nc=16, 
+                   method = "average", index = "ch")
+
+Nb.complete
+plot(Nb.complete$All.index, xlab="number of clusters", ylab="Calinski and Harabasz index")
+
+single.dend <- as.dendrogram(spe.hel.complete)
+plot(single.dend)
+#Unconstrained ordination
+    #CA
+      
+#Constrained ordination 
+    # CCA
+        
             
